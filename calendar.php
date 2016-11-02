83187a98d0e5c684d6af175adce503f7f959337b
xkrtack <edwin-tack@uiowa.edu>
2016-11-02 17:43:08 -0500

    Add files via upload

---
+++ calendar.php
@@ -0,0 +1,79 @@
+
+<html>
+    <head>
+        <title>Calendar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap/min.js"></script>
+        
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.theme.min.css">
+    </head>
+    <body>
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="page-header">
+                    <h1>Calendar</h1>
+                    </div>
+                </div> 
+            </div>
+            
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                        <ul class="nav nav-pills">
+                            <li class="active"><a href="home.html"><span class="glyphicon glyphicon-calendar"></span> &nbsp; Calendar</a></li>
+                            <li><a href="university.html"><span class="glyphicon glyphicon-user"></span> &nbsp; People</a></li>
+                            <li><a href="travel.html"><span class="glyphicon glyphicon-list-alt"></span> &nbsp; News</a></li>
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-sm-9 col-xs-12">
+                    
+                    <p>
+                        Calendar
+                    </p>
+                    <iframe src="https://calendar.google.com/calendar/embed?src=xkrtack%40gmail.com&ctz=America/Chicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                </div>
+                
+                <div class="col-sm-3 col-xs-12">
+                    
+                    <div class="palen panel-default">
+                        <div class="panel-heading">
+                        Upcoming events
+                        </div>
+                        <div class="panel-body">
+                            Please contact administrator for more information
+                        </div>
+                    </div>
+                </div>
+                 
+            </div>   
+            
+            
+            
+            <div class="row">
+                <div class="col-xs-12">
+                    
+                    <div class="panel panel-default">
+                        <div class="panel panel-body">
+                            Administrator contact information: something@uiowa.edu
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div> 
+        
+    
+    </body>
+
+</html>
