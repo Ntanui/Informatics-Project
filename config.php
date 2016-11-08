fc49e0e4127211e82036648265cbd865a56808a3
Ntanui <norman-tanui@uiowa.edu>
2016-11-08 16:32:58 -0600

    Norman Config.php

--- config.php
+++ config.php
@@ -1,12 +1,20 @@
 <?php
-	$dbUser = "hourcade";
-	$dbPassword = "Z4H7yorrKWW3";
-	$dbName = "db_hourcade";
-	$dbHost = "dbdev.cs.uiowa.edu";
-	$adminEmail = "juanpablo-hourcade@uiowa.edu";
-	$baseURL = "http://webdev.divms.uiowa.edu/~hourcade/phpdb";
-	
-	// you should probably set up a table where you enter this info
-	$siteName = "Soccer Lover's Club";
-	$footerText = "Copyright &copy; 2016 Soccer Lover's Club"
+// Site configuration.
+
+// Server information.
+$Proto = "http://";
+$Host = $_SERVER['SERVER_NAME'];
+
+// Title to use in browser title bar.
+$Title = "create site";
+$Name = "Norman Tanui";
+$Email = "norman-tanui@uiowa.edu";
+
+
+// DB connection (from  mysql_db_info file).
+$DBUser = "ntanui";
+$DBName = "db_ntanui";
+$DBHost = "dbdev.cs.uiowa.edu";
+$DBPasswd = "uAqsJ2oke3Ka";
+$baseUrl = "http://webdev.divms.uiowa.edu/~ntanui/phpdb";
 ?>
