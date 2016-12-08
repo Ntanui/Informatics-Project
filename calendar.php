9ae906d7c9754604343c2bd470cf94d33df999d5
xkrtack <edwin-tack@uiowa.edu>
2016-12-08 17:53:07 -0600

    Add files via upload

--- calendar.php
+++ calendar.php
@@ -11,6 +11,19 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.theme.min.css">
     </head>
     <body>
+<div class="container" style="width: 1024px">
+
+<!-- if you have a site table, you'd get this from there -->
+<div class="row">
+    <div class="col-xs-10">
+        
+    </div>
+    <div class="col-xs-2">
+        <a href="login.php">Log In</a>
+        <a>or</a>
+        <a href="login.php">Log Out</a>
+    </div>
+</div>
         <div class="container" style="width: 1024px">
         
         <div class="row">
@@ -33,11 +46,11 @@
                     <div class="navbar navbar-inverse">
                         <div class="container-fluid">
                         <ul class="nav nav-pills">
-                            <li><a href="input.php"><span class="glyphicon glyphicon-home"></span> &nbsp; Edit Page</a></li>
+                            <li><a href="about.php"><span class="glyphicon glyphicon-home"></span> &nbsp; Organization</a></li>
+                            <li><a href="input.php"><span class="glyphicon glyphicon-flag"></span> &nbsp; Edit Page</a></li>
                             <li><a href="people.php"><span class="glyphicon glyphicon-user"></span> &nbsp; People</a></li>
                             <li><a href="news.php"><span class="glyphicon glyphicon-list-alt"></span> &nbsp; News</a></li>
                             <li class="active"><a href="calendar.php"><span class="glyphicon glyphicon-calendar"></span> &nbsp; Calendar</a></li>
-                            <li><a href="about.php"><span class="glyphicon glyphicon-flag"></span> &nbsp; Administrator</a></li>
                         </ul>
                         </div>
                     </div>
@@ -46,11 +59,105 @@
             
             <div class="row">
                 <div class="col-sm-9 col-xs-12">
+
+                
+<?php
+//
+// Code to handle input from the form
+//
+if (isset($_POST['submit'])) {
+    //Run only if the form was submitted
+    
+    //Get data from the form
+    $orgName = $_POST['orgName'];
+    $caLink = $_POST['caLink'];
+    
+    if (!$db){
+        // Connect to database
+        $db = connectDB($dbHost, $dbUser, $dbPassword, $dbName);
+    }
+    
+    
+    //Checking  for input into required fields    
+    $isComplete = true;
+    $errorMessage = "";
+    
+    if (!$email) {
+        $errorMessage .= "Please enter your organization name.";
+        $isComplete = false ;   
+    }   else {
+            $email = makeStringsafe($db, $email);
+    }
+        
+    if (!$password) {
+        $errorMessage .= "Please enter your calendar link.";
+        $isComplete = false ;   
+    }  
+              
+             
+    if (!isComplete) {
+        punt ($errorMessage);
+    }
+    
+      
+    //check if there's a user with the same email
+    $query= " SELECT * FROM news WHERE orgName = '" . $orgName . "';";
+    $result=  queryDB($query, $db);
+    if (nTuples ($result) > 0 ) {
+        // This means a director with the same email.
+        punt ("Sorry we have already have a calendar link for an organization named " . $orgName );
+   
+    }
+    
+    
+    // Sql code to insert tuple or record
+    $insert = "INSERT INTO organization(orgName,caLink) VALUES ('" . $orgName . "' , '" . $caLink . "');";
+    
+    
+    //run the insert statement
+    $result = queryDB($insert, $db);
+    
+    //If no errors, record has been succesfully Inserted.
+    
+    echo ("Succesfully entered a calendar " . $orgName."'s link in the Database");
+    
+        }
+           
+?>
+<!-- visible title-->
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Insert Calendar</h1>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-xs-12">
+                <form action= "calendar.php" method ="post">
+                
                     
+                <!-- title -->
+                    <div class="form-group">
+                        <label for="email"> Name </label>
+                        <input type="text" class="form-control" name ="newstitle"/>
+                    </div>
+                    
+                <!-- news -->
+                    <div class="form-group">
+                        <label for=""> Personal Calendar address </label>
+                        <input type="text" class="form-control" name ="newstitle"/>
+                    </div>
+                
+                        
+                    <button type="submit" class="btn btn-default col-xs-12" name ="submit">Post</button>
+                    
+                </form>
+            </div>
+
+        </div>
                     <p>
                         Calendar
                     </p>
-                    <iframe src="https://calendar.google.com/calendar/embed?src=8krmejpp3n310edoubgm43fu44%40group.calendar.google.com&ctz=America/Chicago" style="border: 0" width="750" height="600" frameborder="0" scrolling="no"></iframe>
                 </div>
                 
                 <div class="col-sm-3 col-xs-12">
@@ -81,7 +188,40 @@
             </div>
             
         </div> 
+
+<div class = "row">
+    <div class ="col-xs-12">
+<table class = "table table-hover">
+    
+    <!-- Headers for table -->
+
+    
+    <tbody>
+<?php
+    if (!$db){
+        // Connect to database
+        $db = connectDB($dbHost, $dbUser, $dbPassword, $dbName);
+    }
+    // Set up query to get all records from users table.
+    $query = "SELECT * FROM organization ORDER BY orgName;";
+    
+    //Run the query
+    $result = queryDB($query, $db);
+    
+    while($row = nextTuple($result)){
+        //Each time the while loop runs,we create one row in the table.
+        echo "\n <tr>";
+        echo "<td>" . $row['orgName'] ."</td>" ;
+        echo "<td>" . $row['caLink'] . "</td>" ;
+        echo "</tr>";
+    }
+?>
+    </tbody>   
+</table>
         
+    </div>
+    
+</div>
     
     </body>
 
