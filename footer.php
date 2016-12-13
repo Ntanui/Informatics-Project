89f57b546ad21659b94ce0633974f7fa00a748dc
Ntanui <norman-tanui@uiowa.edu>
2016-12-13 16:31:11 -0600

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
 <div class="footer">
     <div class="col-xs-12">
         <div class="panel panel-default">
-			<address>
-			2016 Group 3 Informatics <br>
-			Norman Tanui and Edwin Tack<br>
-			Norman-tanui@uiowa.edu<br>
-			Edwin-tack@uiowa.edu<br>
-			555-555-555<br>	
-            </div>
+			<b>Norman Tanui and Edwin Tack</b><br>
+			<b>Norman-tanui@uiowa.edu</b><br>
+			<b>Edwin-tack@uiowa.edu</b><br>
+			<b>555-555-555</b><br>	
         </div>
     </div>
 </div>
