acac129a34fc5050af6ff56ef12052ee6fbe7035
xkrtack <edwin-tack@uiowa.edu>
2016-12-13 14:45:02 -0600

    Add files via upload

--- email.php
+++ email.php
@@ -10,15 +10,61 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.theme.min.css">
 </head>
+    <style>
+    input[type=text], select {
+        width: 100%;
+        padding: 12px 10px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    
+    input[type=submit] {
+        width: 100%;
+        background-color: #2E9AFE;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+    
+    input[type=reset] {
+        width: 100%;
+        background-color: #2E9AFE;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+    
+    input[type=submit]:hover {
+        background-color: #2E9AFE;
+    }
+    input[type=reset]:hover {
+        background-color: #2E9AFE;
+    }
+    
+    div {
+        border-radius: 5px;
+        background-color: #f2f2f2;
+        padding: 2px;
+    }
+    </style>
 <body>
 
 <h2>Send e-mail to Administrator</h2>
 
-<form action="mailto:administrator@gmail.com" method="post" enctype="text/plain">
+<form action="mailto:edwin-tack@uiowa.edu" method="post" enctype="text/plain">
 Your Name:<br>
-<input type="text" name="name"><br>
+<input type="text" name="name" size="50"><br>
 Your E-mail:<br>
-<input type="text" name="mail"><br>
+<input type="text" name="mail" size="50"><br>
 Comment:<br>
 <textarea class="form-control" name="comment" id="comment" rows="5"></textarea>
 <input type="submit" value="Send">
